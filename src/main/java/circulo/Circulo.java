35509a4962bf4b9743d1453a4d44a9879e995185
cristian <cristiangalvezcaravaca@gmail.com>
2022-02-22 10:24:42 +0100

    Validando el proyecto circulo

---
+++ src/main/java/circulo/Circulo.java
@@ -0,0 +1,22 @@
+package circulo;
+import java.util.Scanner;
+/**
+*
+* @author monse
+*/
+public class Circulo {
+ /**
+ * @param args the command line arguments
+ */
+ public static void main(String[] args) {
+ // TODO code application logic here
+ final double PI=3.1416;
+ // TODO Apéndice de método generado automáticamente
+Scanner s=new Scanner(System.in);
+System.out.println("Introduce el radio:");
+double r=s.nextDouble();
+double area=(PI*r*r);
+System.out.printf("El valor del área es %f\n", area);
+
+ }
+}
